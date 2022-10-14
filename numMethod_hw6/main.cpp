9d7eee6a2d4a8cc0a2a38a93da051646a6683f7b
Irokanade <michaeljs.leong@gmail.com>
2022-10-14 14:27:22 +0800

    Initial Commit

---
+++ numMethod_hw6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  numMethod_hw6
+//
+//  Created by Michael Leong on 14/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
